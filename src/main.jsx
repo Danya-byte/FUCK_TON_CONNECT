ae274587832981837c2ada562515be396ab86bb5
Ivashin <danya001406@gmail.com>
2025-02-23 03:27:04 +0300

    Danya update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl="https://gist.githubusercontent.com/alexcraviotto/b5d974bc120c3b46a0d047ba79cb4874/raw/2b04202d388d9547173b56c6ef27734edba18b29/tonconnect-manifest.json"
+    <TonConnectUIProvider manifestUrl="https://api.jsonserve.com/TyBLV6"
     >
       <App />
     </TonConnectUIProvider>
